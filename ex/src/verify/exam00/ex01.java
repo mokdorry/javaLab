b2d0eecf71f7436ae1267774e30a6b0497ae22cb
mokdorry <p00p19@naver.com>
2021-04-13 18:03:33 +0900

    ex (2021.04.13)

--- ex/src/verify/exam00/ex01.java
+++ ex/src/verify/exam00/ex01.java
@@ -4,7 +4,7 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¾È³çÇÏ¼¼¿ä!!");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”!!");
 	}
 
 }
